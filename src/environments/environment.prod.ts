d14d223970f7e7713cd873145d68914bf5f16cfc
Todd Risenmay <toddriz@gmail.com>
2017-12-13 07:49:19 -0700

    included built files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBYiv39ThRWIIwav7hgUpTDHfBN983w2jM',
+    authDomain: 'createcool-8795b.firebaseapp.com',
+    databaseURL: 'https://createcool-8795b.firebaseio.com',
+    projectId: 'createcool-8795b',
+    storageBucket: 'createcool-8795b.appspot.com',
+    messagingSenderId: '173828790102'
+ }
 };
